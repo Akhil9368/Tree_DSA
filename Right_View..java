9ef0572988f034299f996488d8764da4832adf01
Akhil Soni <85015174+Akhil9368@users.noreply.github.com>
2022-07-17 23:41:33 +0530

    Update Right_View..java

--- Right_View..java
+++ Right_View..java
@@ -10,3 +10,25 @@
          Right_View(root.right,level+1);
          Right_View(root.left,level+1);
    }
+void Right_View_Iterative(binary<Integer> root){
+       Queue<binary> Que = new LinkedList<>();
+       if(root == null){
+           return;
+       }
+       int count=0;
+       Que.add(root);
+       while (!Que.isEmpty()){
+           count= Que.size();
+           for(int i=0;i<count;i++) {
+               binary<Integer> node = Que.poll();
+               if(i==count-1)
+                   System.out.print(node.data+" ");
+
+               if (node.left != null)
+                   Que.add(node.left);
+
+               if (node.right != null)
+                   Que.add(node.right);
+           }
+       }
+   }
