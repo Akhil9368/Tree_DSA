b74bc3d9ee1d0baadb580fa32b8b60c20277da5f
Akhil Soni <85015174+Akhil9368@users.noreply.github.com>
2022-07-19 00:58:15 +0530

    Create Top_View.java

---
+++ Top_View.java
@@ -0,0 +1,31 @@
+void Top_View(binary<Integer> root){
+       int distance =0;
+
+       TreeMap<Integer, ArrayList<Integer>> map = new TreeMap<>();
+       Vertical_Order_Helper(root,distance,map);
+       // To traverse the map elements
+       for(Map.Entry<Integer,ArrayList<Integer>> m :map.entrySet()){
+           ArrayList<Integer> arr = m.getValue();
+           System.out.print(arr.get(0)+" ");
+       }
+
+   }
+
+    private void Vertical_Order_Helper(binary<Integer> root, int distance, TreeMap<Integer, ArrayList<Integer>> map) {
+         if(root == null)return;
+         if(map.get(distance)==null){
+             ArrayList<Integer> arr = new ArrayList<>();
+             arr.add(root.data);
+             map.put(distance,arr);
+         }
+         else
+         {
+             ArrayList<Integer> arr = map.get(distance);
+             arr.add(root.data);
+             map.put(distance,arr);
+         }
+
+         Vertical_Order_Helper(root.left,distance-1,map);
+         Vertical_Order_Helper(root.right,distance+1,map);
+
+    }
